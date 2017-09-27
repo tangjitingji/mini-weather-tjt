13c74a3ad7c0dd758b2060e863801963d6cab357
tangjitingji <tangjiting@pku.edu.cn>
2017-09-27 18:18:18 +0800

    the first commit

---
+++ app/src/main/java/com/example/tangjiting/miniweather/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tangjiting.miniweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by tangjiting on 2017/9/25.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
+
+
+
